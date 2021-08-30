62eea61965766bf10d5197bcf38f18d49c8efe40
Hamzah <hamzah@ualberta.ca>
2021-08-29 19:30:59 -0600

    initial commit

---
+++ src/main/java/com/covid/covidafrica/CovidAfricaApplication.java
@@ -0,0 +1,18 @@
+package com.covid.covidafrica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/*
+1. when app runs, get data from url
+
+*/
+@SpringBootApplication
+public class CovidAfricaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidAfricaApplication.class, args);
+	}
+
+}
