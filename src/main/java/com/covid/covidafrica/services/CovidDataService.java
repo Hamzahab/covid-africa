46378501e8992970a22f98068dbad775199d76f2
Hamzah <hamzah@ualberta.ca>
2021-08-30 17:27:13 -0600

    refactored, parsed for all african countries

--- src/main/java/com/covid/covidafrica/services/CovidDataService.java
+++ src/main/java/com/covid/covidafrica/services/CovidDataService.java
@@ -12,6 +12,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import com.covid.covidafrica.models.LocationStats;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators.None;
 
 import org.apache.commons.csv.CSVFormat;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -27,11 +28,13 @@ class will get data from database (given url), and parse for african covid data
 @Service
 public class CovidDataService{
 
-    
-    private static String dataURL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
-    
 
-    private List<LocationStats> statsList = new ArrayList<LocationStats>();
+    
+    private static String covidStatsURL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    private static String africanCountURL = "https://raw.githubusercontent.com/dbouquin/IS_608/master/NanosatDB_munging/Countries-Continents.csv";
+
+    private static List<LocationStats> statsList = new ArrayList<LocationStats>();
+    
     //when application starts up, execute
     @PostConstruct
     /*
@@ -41,39 +44,22 @@ public class CovidDataService{
     */
     @Scheduled(cron = "* * 1 * * *")
     public static void getData() throws IOException, InterruptedException{
+
+        // //sends requests and retrieves responses
+        // HttpClient client = HttpClient.newHttpClient();
+        // HttpRequest request = HttpRequest.newBuilder().uri(URI.create(covidStatsURL)).build();
+
+        //get response for both african countries data and covidData
+        HttpResponse<String> covidStatsResponse = getHTTPResponse(covidStatsURL);
+        HttpResponse<String> africanCountriesResponse = getHTTPResponse(africanCountURL);
+
         //temporary, will end up populating statsList
-        List<LocationStats> currentStats = new ArrayList<LocationStats>();
-
-        //sends reuquests and retrieves responses
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(dataURL)).build();
-
-        //send request
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        StringReader csvReader = new StringReader(response.body());
-
-        //parse all csv data into vars
-        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader() .parse(csvReader);
-        HashSet<String> hs = new HashSet<>();
-        int totalAllTime = 0;
-        for (CSVRecord record : records) {
-            String provState = record.get("Province/State");
-            String countRegion = record.get("Country/Region");
-            int currentTotal = Integer.valueOf(record.get(record.size() - 1));
-
-            if(!countRegion.equals("")){
-                LocationStats newInst = new LocationStats(provState, countRegion, currentTotal, getTotal(record));
-
-                currentStats.add(newInst);
-                hs.add(countRegion);
-            }
-        }
-        //test output
-        //System.out.println(currentStats.get(0).getCountry() + " " + currentStats.get(0).getCurrentTotal() + " " + currentStats.get(0).getTotalAllTime());
+        List<LocationStats> currentDayStats = parseCovidData(covidStatsResponse, parseAfricanCountries(africanCountriesResponse));
+        statsList = currentDayStats;
+       
     }
 
-    public static int getTotal(CSVRecord record){
+    public static int getTotalCases(CSVRecord record){
         int completeTotal = 0;
         for(int i = 4; i < record.size() ;i++){
             completeTotal += Integer.valueOf(record.get(i));
@@ -81,5 +67,53 @@ public class CovidDataService{
         return completeTotal;
     }
 
+    public static HttpResponse<String> getHTTPResponse(String url) throws IOException, InterruptedException{
 
+        //send reuquest and retrieve responses
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+
+        //get response
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+
+    public static List<LocationStats> parseCovidData(HttpResponse<String> response, HashSet<String> africanCountriesList) throws IOException{
+        StringReader csvReader = new StringReader(response.body());
+        List<LocationStats> currentStats = new ArrayList<LocationStats>();
+
+        //parse all csv data into vars
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader() .parse(csvReader);
+        for (CSVRecord record : records) {
+            String provState = record.get("Province/State");
+            String countRegion = record.get("Country/Region");
+            int currentTotal = Integer.valueOf(record.get(record.size() - 1));
+
+            if(!countRegion.equals("") && africanCountriesList.contains(countRegion)){
+                LocationStats newInst = new LocationStats(provState, countRegion, currentTotal, getTotalCases(record));
+
+                currentStats.add(newInst);
+                System.out.println(newInst.getCountry() + " " + newInst.getCurrentTotal() + " " + newInst.getTotalAllTime());
+
+            }
+        }
+        return currentStats;
+    }
+
+    public static HashSet<String> parseAfricanCountries(HttpResponse<String> response) throws IOException{
+        StringReader csvReader = new StringReader(response.body());
+        HashSet<String> countries = new HashSet<>();
+        //parse all csv data into vars
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader() .parse(csvReader);
+
+        for (CSVRecord record : records) {
+            String continent = record.get("Continent");
+            //if country is africa
+            if(continent.equals("Africa")){
+                String country = record.get("Country");
+                countries.add(country);
+            }
+        }
+        return countries;
+    }
 }
